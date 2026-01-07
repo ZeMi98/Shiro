75f476d1cceda43a785b0fbba81594bfa1399c7c
liuzeming1 <liuzeming@storswift.com>
2026-01-07 21:30:44 +0800

    feat: ignore live2d assets in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default defineConfig(
   {
     formatting: false,
     lessOpinionated: true,
-    ignores: [],
+    ignores: ['public/assets/live2d/**'],
     preferESM: false,
     react: 'next',
     tailwindCSS: true,
